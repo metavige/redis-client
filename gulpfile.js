26e2f6c440de28cfba0de5db3d4754be50623df0
Ricky Chiang <metavige@gmail.com>
2014-11-12 10:37:16 +0800

    add gulp test support

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var batch = require('gulp-batch');
 
-gulp.task('default', function() {
+gulp.task('default', function () {
     //     return gulp.src('test/**/*.js', {
     //             read: false
     //         })
@@ -10,14 +10,19 @@ gulp.task('default', function() {
     //             reporter: 'list'
     //         }));
 
-    gulp.watch(['test/**', 'lib/**'], batch(function(events, cb) {
-        return gulp.src(['test/**/*.js'])
-            .pipe(mocha({
-                reporter: 'list'
-            }))
-            .on('error', function(err) {
-                console.log(err.stack);
-            });
+    gulp.watch(['test/**', 'lib/**'], batch(function (events, cb) {
+        return TestSpec();
     }));
-
 });
+
+gulp.task('test', function () {
+    return TestSpec();
+});
+
+function TestSpec() {
+    return gulp.src(['test/**/*.js'])
+        .pipe(mocha())
+        .on('error', function (err) {
+            console.log(err.stack);
+        });
+}
