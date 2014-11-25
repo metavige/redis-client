58bb9ff7a7bd3d3c8835805bcd0f7553a3f602a7
metavige <metavige@gmail.com>
2014-11-25 13:38:58 +0800

    change config.json path

--- app/lib/config.js
+++ app/lib/config.js
@@ -9,7 +9,7 @@ console.log('start agent.....');
 // read conf/config.json
 try {
     // console.log(__dirname);
-    var data = fs.readFileSync(__dirname + '/../conf/config.json', 'utf8');
+    var data = fs.readFileSync(__dirname + '/../../conf/config.json', 'utf8');
     config.settings = JSON.parse(data.toString());
 } catch (err) {
     throw err;
