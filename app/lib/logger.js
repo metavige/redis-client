c71703354a91bae069ed344ca9d1b7792eb28a79
metavige <metavige@gmail.com>
2014-12-17 13:35:19 +0800

    change log path and quiet nodemon message

--- app/lib/logger.js
+++ app/lib/logger.js
@@ -10,10 +10,6 @@ var logger = new(winston.Logger)({
     transports: [
         new(winston.transports.Console)({
             level: 'debug'
-        }),
-        new(winston.transports.File)({
-            filename: path.join(__dirname, '../../logs/agent.log'),
-            level: 'debug'
         })
     ]
 });
