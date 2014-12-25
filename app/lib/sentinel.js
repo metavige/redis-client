8fc120224ebdf695f9ce7562b1d345ccd651ed81
metavige <metavige@gmail.com>
2014-12-25 16:19:50 +0800

    在 switch master 的時候，去呼叫 ContainerApi 變更
    目前其他事件都只是 log, 沒有呼叫 containerApi 做動作
    之後會加上自動回復的動作～

--- app/lib/sentinel.js
+++ app/lib/sentinel.js
@@ -8,36 +8,37 @@
  */
 
 var path = require('path'),
-  Sentinel = require('node-sentinel'),
-  logger = require(path.join(__dirname, './logger')),
-  config = require(path.join(__dirname, 'lib/config'));
+    Sentinel = require('node-sentinel'),
+    logger = require(path.join(__dirname, './logger')),
+    config = require(path.join(__dirname, 'lib/config'));
 
 if (config.isProxy()) {
 
-  var sentinel = new Sentinel('127.0.0.1', '6380');
+    var containerApi = require(path.join(__dirname, './container'));
+    var sentinel = new Sentinel('127.0.0.1', '6380');
 
-  sentinel.ping(function(err, pong) {
-    if (err != null) {
-      logger.error('ping sentinel error:', err);
-    }
-    logger.debug('PING Sentinel: ', pong);
-  });
+    sentinel.ping(function(err, pong) {
+        if (err != null) {
+            logger.error('ping sentinel error:', err);
+        }
+        logger.debug('PING Sentinel: ', pong);
+    });
 
-  // 接聽 slave 設定的狀態變更
-  sentinel.on('slave-reconf-status', function (data) {
-    logger.debug('[sentinel][slave-reconf]', data);
-  })
-  
-  // 接聽 failover 狀態變更事件
-  sentinel.on('failover-status', function (data) {
-    logger.debug('[sentinel][failover]', data);
-  })
+    // 接聽 slave 設定的狀態變更
+    sentinel.on('slave-reconf-status', function(data) {
+        logger.debug('[sentinel][slave-reconf]', data);
+    })
 
-  // 接聽 master 變更的事件，這是主要用來通知 ContainerApi 的事件
-  sentinel.on('switch-master', function(event) {
-    logger.debug('switch-master:', event);
-    
-    
-  });
+    // 接聽 failover 狀態變更事件
+    sentinel.on('failover-status', function(data) {
+        logger.debug('[sentinel][failover]', data);
+    })
+
+    // 接聽 master 變更的事件，這是主要用來通知 ContainerApi 的事件
+    sentinel.on('switch-master', function(event) {
+        logger.debug('switch-master:', event);
+
+        containerApi.switchMaster(event.details);
+    });
 
 }
